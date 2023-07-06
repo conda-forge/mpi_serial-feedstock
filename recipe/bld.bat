931d085fcbde0349fbb435934d5971b2ecfa32c5
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-07-06 16:22:26 +0200

    Add FFLAGS to FCFLAGS

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,8 @@ set "cwd=%cd%"
 
 set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
 
+set "FCFLAGS=%FFLAGS%"
+
 bash -lc "echo 'none /tmp usertemp binary,posix=0 0 0' >>/etc/fstab"
 bash -lc "mount"
 rem bash -lc "autoreconf -fvi"
--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ export INSTALL="install"
 export MKINSTALLDIRS="mkdir -p"
 export libdir="${PREFIX}/lib"
 export includedir="${PREFIX}/include"
+export FCFLAGS="${FFLAGS}"
 
 ./configure --prefix=${PREFIX}
 make
