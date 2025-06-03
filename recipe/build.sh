7967b62b82194c014749c1e364865a7357df14ba
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-06-03 14:03:07 +0200

    Return to pre 2.5.0 build system

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,13 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 set -xe
 
-export AR="${AR}"
+export AR="${AR} cruv"
 export INSTALL="install"
+export MKINSTALLDIRS="mkdir -p"
+export libdir="${PREFIX}/lib"
+export includedir="${PREFIX}/include"
 export FCFLAGS="${FFLAGS}"
 
-autoreconf -iv
 ./configure --prefix=${PREFIX}
 make
 make install
